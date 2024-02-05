c89c96eafbd031fa7560610dab2fbb538339c085
SerhiiRud <carmagsales@gmail.com>
2024-02-05 20:05:21 +0200

    image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
       as="section"
       id="hero"
       h={'690px'}
-      bgImage="url('../../images/jpg/Frame.png')"
+      bgImage="url('../images/jpg/Frame.png')"
       backgroundPosition={'center'}
       backgroundRepeat="no-repeat"
       backgroundSize="cover"
